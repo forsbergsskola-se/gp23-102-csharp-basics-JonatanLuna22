311cbe440b9fdd27d27982b82f72faa1cbaa92f2
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-04 16:35:21 +0200

    Done

---
+++ P3PrintingOutput/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, Output!");
+Console.WriteLine("Welcome to 120 CShar Basics!");
+Console.WriteLine("It's easy to print output...");
+Console.WriteLine("...to the console!");
+    
