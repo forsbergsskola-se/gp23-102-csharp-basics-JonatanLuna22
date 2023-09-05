c7cf019ef7d40ab5f6942264f22536f3230f2181
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-05 09:33:06 +0200

    Fix Text

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, Output!");
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to 120 CShar Basics!");
 Console.WriteLine("It's easy to print output...");
 Console.WriteLine("...to the console!");
+
     
