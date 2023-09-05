7c8df4560cee191fb10e9d1829c716364caf6dda
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-05 11:04:05 +0200

    Add Variables

---
+++ P5Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// int for numbers
+// string for letters
+
+// declaration
+string kingPosition;
+
+// initialization (first assignment)
+kingPosition = "D1";
+
+// access
+Console.WriteLine(kingPosition);
+// reassignment (second assignment)
+
+kingPosition = "D2";
+// access
+Console.WriteLine(kingPosition);
+
+string KingPosition = "D1"; 
+Console.WriteLine(KingPosition);
