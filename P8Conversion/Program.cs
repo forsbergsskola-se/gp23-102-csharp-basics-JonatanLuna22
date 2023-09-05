79a3873786e59f2ec0865bb9e79dd31657e614fa
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-05 16:16:13 +0200

    Create P8

--- P8Conversion/Program.cs
+++ P8Conversion/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Give me a number");
+string number = Console.ReadLine();
+double d = Convert.ToDouble(number);
+Console.WriteLine($"{d}");
+int i = Convert.ToInt32(d);
+Console.WriteLine($"{i}");
+int i = Convert.ToInt32(number);
