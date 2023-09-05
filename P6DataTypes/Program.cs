41b89929056ed96ebd15e5a6d92e48a8b7e7bcaa
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-05 15:15:34 +0200

    Fix till 6

--- P6DataTypes/Program.cs
+++ P6DataTypes/Program.cs
@@ -4,3 +4,17 @@ using System.IO.Pipes;
 
 string outputString = "Hello World";
 Console.WriteLine(outputString);
+
+float decimalValue = 0.3f;
+Console.WriteLine(decimalValue);
+
+bool isAwesome = true;
+Console.WriteLine(isAwesome);
+
+byte hexByte = 0xa7;
+Console.WriteLine(hexByte);
+
+Console.WriteLine($@"{outputString} {decimalValue} {isAwesome} {hexByte}");
+
+
+
