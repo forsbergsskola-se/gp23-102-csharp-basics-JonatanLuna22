f228e70b80bc0930a2f74aeef221760abbb6bd8c
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-05 10:24:06 +0200

    Create Comments

---
+++ P4Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// JonatanLuna
+// 2023-09-05
+// jonatan.luna@student.forsbergsskola.se 
+
+/* JonatanLuna
+2023-09-05
+jonatan.luna@student.forsbergsskola.se */
+
+Console.WriteLine("Hello, World!");
+
