be7eb5fedb9368b0c9ac975818799cfc20dc7ef8
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-05 09:36:38 +0200

    Add Different path

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -5,4 +5,7 @@ Console.WriteLine("Welcome to 120 CShar Basics!");
 Console.WriteLine("It's easy to print output...");
 Console.WriteLine("...to the console!");
 
-    
+Console.WriteLine(@"Hello, World!
+Welcome to 120 CShar Basics!
+It's easy to print output...
+...to the console!");
