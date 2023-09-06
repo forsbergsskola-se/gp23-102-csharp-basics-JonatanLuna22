4836238caf8b9889b6524b44130a207e3fd6016e
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-06 11:44:27 +0200

    Add Playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-float x = 11.89;
-int code = x;
+
+int x = 5 + 5; // x = 10
+int y = x + 5; // y = 15
+int z = y + y; // z = 30
+Console.WriteLine(z + z); // 60
