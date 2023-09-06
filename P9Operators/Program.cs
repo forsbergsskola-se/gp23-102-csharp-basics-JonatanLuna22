78b454cc18f9167a61c7f2f8e5c4dd0dd3e412d7
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-06 13:46:59 +0200

    Create m to s

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hej! Here You can covert minutes to seconds. Give it a try!");
+string minutes = Console.ReadLine();
+int m = int.Parse(minutes);
+int s = m * 60;
+Console.WriteLine($"{minutes} minutes are {s} seconds");
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -5,3 +5,4 @@ int x = 5 + 5; // x = 10
 int y = x + 5; // y = 15
 int z = y + y; // z = 30
 Console.WriteLine(z + z); // 60
+
