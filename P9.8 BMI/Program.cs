141cfdeee92ca8475f6a92c85144281020bc4b41
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-07 11:15:55 +0200

    Create P9.9 Hypotenuse

---
+++ P9.8 BMI/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine(@"Hej! Here you can calculate your BMI
+
+How much do you weight in kilograms?");
+
+string answer1 = Console.ReadLine();
+float w = float.Parse(answer1);
+
+Console.WriteLine("And how tall are you in meters?");
+
+string answer2 = Console.ReadLine();
+float h = float.Parse(answer2);
+
+float r = w / (h * h);
+
+Console.WriteLine($"Your BMI is {r}.");
---
+++ P9.9 Hypotenuse/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine(@"Hej! Let's calculate the hypotenuse of a triangle.);
+
+Give me the length of one of the sides of the triangle.");
+
+string answer1 = Console.ReadLine();
+float s1 = float.Parse(answer1);
+
+Console.WriteLine("And now give me the length of the other one.");
+
+string answer2 = Console.ReadLine();
+float s2 = float.Parse(answer2);
+
+float x = (s1 * s1) + (s2 * s2);
+float h =  MathF.Sqrt(x);
+
+Console.WriteLine($"The hypotenuse of a triangle with a side of {answer1} and a second side of {answer2} is {h}.");
