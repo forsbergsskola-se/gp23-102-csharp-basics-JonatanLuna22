8904d3d38fd9ee1025c00089f6614b4ce9d06924
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-08 17:00:39 +0200

    Create P11Ifelse

--- P11 Ifelse/Program.cs
+++ P11 Ifelse/Program.cs
@@ -17,8 +17,8 @@ else
 */
 
 Console.WriteLine("What's your age?");
-string answer = Console.ReadLine();
-int age = int.Parse(answer);
+string answer1 = Console.ReadLine();
+int age = int.Parse(answer1);
 
 if (age <= 12)
 {
@@ -32,3 +32,28 @@ else
 {
     Console.WriteLine("You are an adult.");
 }    
+
+Console.WriteLine("Give me another integer.");
+string answer2 = Console.ReadLine();
+int number = int.Parse(answer2);
+// define variable
+
+if (number == age)
+{
+    Console.WriteLine("It is the same number than your age.");
+}
+else if (number < age)
+{
+    Console.WriteLine($"Your age is greater than {answer2}.");
+  
+// assign one number to variable
+}
+else
+{
+    Console.WriteLine($"{answer2} is greater than your age.");
+}
+// assign other number to variable
+
+
+
+// you know that the variable contains the maximum
