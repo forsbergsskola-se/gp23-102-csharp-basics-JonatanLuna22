72b3627236d2e9fd4ee395ecb0cb9703ad001097
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-13 16:22:08 +0200

    Complete P13.1 Dollars

---
+++ P13.1 Dollars/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Write a Program that asks how many Dollars the user wants. Then, the program proceeds to printing that many Dollar Symbols.
+
+Console.WriteLine("How many dollars do you want?");
+int dollars = int.Parse(Console.ReadLine());
+
+int counter = 0;
+  
+string sign = "$";
+
+Console.Write("Here is your dollars: ");
+
+Print:
+if (counter < dollars)
+{
+  counter++;
+  Console.Write(sign);
+  goto Print;
+}
+
+/*
+Console.WriteLine("$" * dollars);
+*/
