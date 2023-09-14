1d29e1cc7595b8726f14d3518265c467eda24938
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-14 14:33:22 +0200

    Complete P13 Go to

--- P13 Go to/Program.cs
+++ P13 Go to/Program.cs
@@ -30,19 +30,33 @@ if myNumber is equal to guess then
 end if
 
 */
-int Rnumber = Random.Shared.Next(0, 10);
+int Rnumber = Random.Shared.Next(0, 100);
 
-Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
+Console.WriteLine("I have picked a number (1-100). You have 10 tries to guess it!");
+
+int tries = 0;
 
 Again:
 int Pnumber = int.Parse(Console.ReadLine());
+tries++;
 
-if (Pnumber == Rnumber)
-{Console.WriteLine($"Congratulation {Pnumber} is the number I picked. You win!");}
+if (tries < 10)
+{
+   if (Pnumber == Rnumber)
+   {
+      Console.WriteLine($"Congratulation {Pnumber} is the number I picked. You win!");
+   }
 
-else if (Pnumber < Rnumber)
-{Console.WriteLine("Nope! My number is greater!");}
+   else if (Pnumber < Rnumber)
+   {
+      Console.WriteLine($"Nope! My number is greater! You have {10 - tries} tries left.");
+   }
 
-else
-{Console.WriteLine("Nope! My number is smaller!");}
-goto Again;
+   else
+   {
+      Console.WriteLine($"Nope! My number is smaller! You have {10 - tries} tries left.");
+   }
+
+   goto Again;
+}
+Console.WriteLine("You run out of tries. Game over!");
