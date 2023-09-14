3af7df5e5c22b6efa84f8a88a57cb6bad5be5a87
adamviger <75254051+adamviger@users.noreply.github.com>
2023-09-14 13:51:47 +0200

    Complete P13.3 Pattern

--- P13.3 Pattern/Program.cs
+++ P13.3 Pattern/Program.cs
@@ -14,14 +14,17 @@ Input: 5
 Console.WriteLine("Hej! I will create a pattern for you. Give me a whole number and that will be the size of the pattern (x times x).");
 int sharps = int.Parse(Console.ReadLine());
   
-string sign = "#";
+string sign = "#-";
+string sign2 = "-#";
 
 Console.WriteLine("Here is your pattern:");
 
+int counter5 = 0;
 int counter2 = 0;
+int counter3 = 0;
 PrintRow:
 
-if (sharps > 0)
+if (sharps > 0 && sharps > counter5) 
 {
     int counter = 0;
     Print:
@@ -31,10 +34,29 @@ if (sharps > 0)
         Console.Write(sign);
         goto Print;
     }
-    if (counter2 < sharps -1)
-    {
+     Console.WriteLine();
+     if (counter2 < sharps -1)
+     { 
+        counter5++;
         counter2++;
-        Console.WriteLine();
+    }
+}
+
+if (sharps > 0 && sharps > counter5)
+{
+    int counter4 = 0;
+    Print2:
+    if (counter4 < sharps)
+    {
+        counter4++;
+        Console.Write(sign2);
+        goto Print2;
+    }
+    Console.WriteLine();
+    if (counter3 < sharps -1)
+    {
+        counter5++;
+        counter3++;
         goto PrintRow;
     }
 }
